8f4c4ab4951e7a6b1813bfe5a0ce6e7cf2a0dd2c
Miguel Fernandes <up201503538@fe.up.pt>
2017-03-24 01:08:16 +0000

    Grande redução do SettingsDialog.

--- Aula 1/src/dkeep/gui/SettingsDialog.java
+++ Aula 1/src/dkeep/gui/SettingsDialog.java
@@ -20,7 +20,6 @@ import java.awt.event.ActionEvent;
 public class SettingsDialog extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
-	
 	private PanelManager pm;
 
 	public static void main(String[] args) {
@@ -43,30 +42,16 @@ public class SettingsDialog extends JDialog {
 		contentPanel.setLayout(null);
 
 		JSlider slider = new JSlider();
-		slider.setBounds(12, 129, 200, 44);
-		slider.setMinimum(1);
-		slider.setMaximum(5);
-		slider.setMajorTickSpacing(1);
-		slider.setPaintTicks(true);
-		slider.setPaintLabels(true);
-		
-		
-		contentPanel.add(slider);
+		initSlider(slider);
 		
 		JLabel lblNumberOfOgres = new JLabel("Number of Ogres");
-		lblNumberOfOgres.setBounds(12, 100, 112, 16);
-		contentPanel.add(lblNumberOfOgres);
+		initLabel(lblNumberOfOgres, 12, 100);
 		
 		JLabel lblGuardPersonality = new JLabel("Guard Personality");
-		lblGuardPersonality.setBounds(12, 13, 112, 16);
-		contentPanel.add(lblGuardPersonality);
+		initLabel(lblGuardPersonality, 12, 13);
 		
 		JComboBox<String> comboBox = new JComboBox<String>();
-		comboBox.setBounds(22, 37, 149, 22);
-		comboBox.addItem("Rookie");
-		comboBox.addItem("Drunk");
-		comboBox.addItem("Suspicious");
-		contentPanel.add(comboBox);
+		initComboBox(comboBox, new String[]{"Rookie", "Drunk", "Suspicious"});
 		
 		JPanel buttonPane = new JPanel();
 		buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
@@ -96,6 +81,29 @@ public class SettingsDialog extends JDialog {
 		buttonPane.add(cancelButton);
 		
 	}
+	public void initSlider(JSlider slider){
+		slider.setBounds(12, 129, 200, 44);
+		slider.setMinimum(1);
+		slider.setMaximum(5);
+		slider.setMajorTickSpacing(1);
+		slider.setPaintTicks(true);
+		slider.setPaintLabels(true);
+		contentPanel.add(slider);
+	}
+	
+	public void initLabel(JLabel label, int x, int y){
+		label.setBounds(x, y, 112, 16);
+		contentPanel.add(label);
+	}
+	
+	public void initComboBox(JComboBox<String> cb, String[] options){
+		cb.setBounds(22, 37, 149, 22);
+		for (int i = 0; i < options.length; i++){
+			cb.addItem(options[i]);
+		}
+		contentPanel.add(cb);
+	}
+	
 	public void setPanelManager(PanelManager pm){
 		this.pm = pm;
 	}
--- Iteração 4/README.txt
+++ Iteração 4/README.txt
@@ -1,2 +1,5 @@
 Esta classe é o antigo GUI para display do mapa em texto.
-Foi substituído, na iteração 5, por outra classe GameGUI.
+
+Foi substituída, na iteração 5, por outra classe GameGUI.
+
+A extensão do ficheiro foi alterada de .java para .txt para não interferir com o resultado do BetterCodeHub.
