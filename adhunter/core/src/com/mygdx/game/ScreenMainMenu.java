634d8da39aab6fdf161231ea24412a818828e9a6
Miguel Fernandes <up201503538@fe.up.pt>
2017-04-04 18:30:20 +0100

    Mudança de implements para extends em classe.

--- adhunter/core/src/com/mygdx/game/ScreenMainMenu.java
+++ adhunter/core/src/com/mygdx/game/ScreenMainMenu.java
@@ -2,6 +2,6 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ScreenAdapter;
 
-public class ScreenMainMenu implements ScreenAdapter {
-    
+public class ScreenMainMenu extends ScreenAdapter {
+
 }
